5845708e44ec2470cd76055d4ff46f4de1cf4228
siokonbu <kousuke.soccer05@gmail.com>
2020-02-02 16:17:14 +0900

    cors error modified

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -13,12 +13,6 @@ def create_app():
 
     init_db(app)
     CORS(app)
-    @app.after_request
-    def after_request(response):
-      response.headers.add('Access-Control-Allow-Origin', '*')
-      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-      return response
 
     return app
 
--- api/api/views/user.py
+++ api/api/views/user.py
@@ -2,9 +2,11 @@ from flask import Blueprint, request, jsonify
 from api.database import db
 from api.models import User
 from api.models import UserSchema
+from flask_cors import CORS
 
 # ルーティング設定
 user_router = Blueprint('user_router', __name__)
+CORS(user_router)
 
 @user_router.route('/users', methods=['GET'])
 def show_users():
@@ -23,16 +25,20 @@ def signup():
     print('-----------')
     print(request.get_json())
     print('-----------')
+    print('-----------')
+    print(request.args)
+    print('-----------')
 
-    post_contents = request.get_json()
-    name = post_contents['name']
-    password = post_contents['password']
-    email = post_contents['email']
-    image_path = post_contents['image_path']
-    profile = post_contents['profile']
-
-    new_user = User(name=name, password=password, email=email, image_path=image_path, profile=profile, is_admin=False)
-    db.session.add(new_user)
-    db.session.commit()
+    # post_contents = request.get_json()
+    # name = post_contents['name']
+    # password = post_contents['password']
+    # email = post_contents['email']
+    # image_path = post_contents['image_path']
+    # profile = post_contents['profile']
+    #
+    # new_user = User(name=name, password=password, email=email, image_path=image_path, profile=profile, is_admin=False)
+    # db.session.add(new_user)
+    # db.session.commit()
 
     # return jsonify({'name': name})
+    return 'got request'
