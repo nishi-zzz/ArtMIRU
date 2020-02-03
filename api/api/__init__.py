0d44c90b592f504f825c0b62b58729a41c4366af
siokonbu <kousuke.soccer05@gmail.com>
2020-02-03 09:34:20 +0900

    POST&GET /arts endpoint done

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response, jsonify
 from .views.user import user_router
+from .views.art import art_router
 from api.database import init_db
 import api.models
 from flask_marshmallow import Marshmallow
@@ -10,6 +11,7 @@ def create_app():
     app.debug = True
     app.config.from_object('api.config.Config')
     app.register_blueprint(user_router, url_prefix='/api')
+    app.register_blueprint(art_router, url_prefix='/api')
 
     init_db(app)
     CORS(app)
@@ -19,4 +21,4 @@ def create_app():
 app = create_app()
 ma = Marshmallow(app)
 
-from api.views import user
+# from api.views import user
---
+++ api/api/views/art.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request, jsonify
+from api.database import db
+from api.models import Art
+from api.models import ArtSchema
+from flask_cors import CORS
+
+art_router = Blueprint('art_router', __name__)
+CORS(art_router)
+
+@art_router.route('/arts', methods=['GET'])
+def show_arts():
+    arts = Art.query.order_by(Art.id.desc()).all()
+    arts_schema = ArtSchema(many=True)
+    return jsonify({'arts': arts_schema.dump(arts)})
+
+@art_router.route('/arts', methods=['POST'])
+def register_art():
+    print('-----------')
+    print(request.get_json())
+    print('-----------')
+
+    post_contents = request.get_json()
+    if isinstance(post_contents, type(None)):
+        return 'json is empty'
+    title = post_contents['title']
+    author = post_contents['author']
+    image_path = post_contents['image_path']
+
+    try:
+        new_art = Art(title=title, author=author, image_path=image_path)
+        db.session.add(new_art)
+        db.session.commit()
+    except:
+        return 'sql error'
+
+    return 'got request'
